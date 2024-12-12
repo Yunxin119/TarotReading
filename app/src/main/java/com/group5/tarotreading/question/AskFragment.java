b2a2d3d22d5dbac4c16bdfdf8692a281be697bb7
Yunxin Liu <1999yunxin@gmail.com>
2024-12-11 19:48:53 -0800

    Prevent guest from doing tarot reading

--- app/src/main/java/com/group5/tarotreading/question/AskFragment.java
+++ app/src/main/java/com/group5/tarotreading/question/AskFragment.java
@@ -1,6 +1,8 @@
 package com.group5.tarotreading.question;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.group5.tarotreading.R;
+import com.group5.tarotreading.user.LoginActivity;
 
 public class AskFragment extends Fragment {
     @Nullable
@@ -18,11 +21,19 @@ public class AskFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ask, container, false);
 
+        SharedPreferences preferences = requireActivity().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        boolean isLoggedIn = preferences.getBoolean("isLoggedIn", false);
+
         Button askButton = view.findViewById(R.id.askquestion);
         // Simply direct to question activity
         askButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), QuestionActivity.class);
-            startActivity(intent);
+            if (!isLoggedIn) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(getActivity(), QuestionActivity.class);
+                startActivity(intent);
+            }
         });
 
         return view;
--- app/src/main/java/com/group5/tarotreading/question/TodayFragment.java
+++ app/src/main/java/com/group5/tarotreading/question/TodayFragment.java
@@ -1,6 +1,8 @@
 package com.group5.tarotreading.question;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,22 +14,30 @@ import android.widget.Button;
 
 import com.group5.tarotreading.R;
 import com.group5.tarotreading.card.CardPickActivity;
+import com.group5.tarotreading.user.LoginActivity;
 
 public class TodayFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_today, container, false);
+        SharedPreferences preferences = requireActivity().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        boolean isLoggedIn = preferences.getBoolean("isLoggedIn", false);
 
         Button todayButton = view.findViewById(R.id.today);
         // Set onclick listener, pass required value like spreadtype, question, num of cards to card picking screen
         todayButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), CardPickActivity.class);
-            intent.putExtra("spreadType", "OneCard");
-            intent.putExtra("question", "Tell me today's fortune, give me suggestion");
-            intent.putExtra("pickcard", 1);
-            intent.putExtra("cutcard", false);
-            startActivity(intent);
+            if (!isLoggedIn) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(getActivity(), CardPickActivity.class);
+                intent.putExtra("spreadType", "OneCard");
+                intent.putExtra("question", "Tell me today's fortune, give me suggestion");
+                intent.putExtra("pickcard", 1);
+                intent.putExtra("cutcard", false);
+                startActivity(intent);
+            }
         });
 
         return view;
